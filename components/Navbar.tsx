9b3b43dcbaf3a93297ab13ba297501e63b3d71f3
Geer68 <germhidalgo@gmail.com>
2025-09-03 11:23:37 -0300

    navbar y textos hero

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,22 @@ import { Container } from "@/layouts/Container";
 /* eslint-disable @next/next/no-img-element */
 export default function Navbar() {
   return (
-    <div className="w-full h-98 fixed top-0 py-4">
-      <Container>
+    <div className="w-full h-98 fixed top-0 py-4 ">
+      <Container className="flex justify-between items-center">
         <img src="/logos/logo.webp" alt="Logo" className="w-24 md:w-32" />
+        <section>
+          <a
+            href="https://sub0.gg"
+            target="_blank"
+            className="select-none rounded-xl px-3 py-3 font-semibold
+                     bg-pink-600/80 text-white shadow-lg ring-1 ring-black/10
+                     hover:bg-pink-500 transition
+                     disabled:opacity-60 disabled:cursor-not-allowed
+                     sm:min-w-[220px]"
+          >
+            What is sub0?
+          </a>
+        </section>
       </Container>
     </div>
   );
--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -8,31 +8,27 @@ export default function Hero() {
       className="w-full min-h-[100dvh] bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: 'url("/backgrounds/hero.webp")' }}
     >
-      <Container className="flex flex-col items-center pt-50 gap-10">
-        <h1 className="font-display leading-[0.95] max-w-6xl text-center">
+      <Container className="flex flex-col items-center pt-50 md:pt-60 gap-10">
+        <h1 className="font-display leading-[0.95] md:max-w-6xl text-center">
           <span className="block text-5xl sm:text-6xl md:text-8xl">
-            Make your ideas
+            ship products{" "}
           </span>
           <span className="block text-5xl font-bold sm:text-6xl md:text-8xl">
-            come true
+            solve problems
           </span>
         </h1>
 
-        <p className="font-body max-w-2xl font-manrope text-base text-center text-white/85 sm:text-lg md:max-w-3xl">
-          Road to Sub0 is a{" "}
-          <span className="font-bold">global, collaborative</span> program that
-          aligns the ecosystem around one{" "}
-          <span className="font-bold">journey</span> to
-          <span className="font-bold font-unbounded"> Sub0</span> and delivers
-          new projects building on Polkadot.
+        <p className="font-body max-w-2xl font-manrope text-base text-center text-white/85 sm:text-xl md:max-w-3xl">
+          One journey, three hackathons. Compete, lauch your demo, and win cash
+          prizes to keep building on Polkadot.
         </p>
 
         <EmailForm />
 
         <div className="mt-10 flex w-full max-w-3xl justify-center gap-8 md:gap-20 md:mt-14">
-          <MetricHero text="+40" subtitle="countries" glowing marginTop />
+          {/* <MetricHero text="+40" subtitle="countries" glowing marginTop /> */}
           <MetricHero text="+100K" subtitle="for creative ideas" glowing />
-          <MetricHero text="3" subtitle="hackathons" glowing marginTop />
+          {/* <MetricHero text="3" subtitle="hackathons" glowing marginTop /> */}
         </div>
       </Container>
     </div>
