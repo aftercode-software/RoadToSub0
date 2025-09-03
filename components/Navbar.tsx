9c857e98b50ce1f078a8540bebe87db35de6e2ae
Gabriel Pérez dIEZ <gaabgames@gmail.com>
2025-09-03 12:22:00 -0300

    cambio

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
           <a
             href="#roadmap"
             className={`
-                inline-flex w-full items-center justify-center rounded-xl px-4 py-3 text-lg font-bold
+                inline-flex w-full items-center justify-center rounded-xl px-4 py-3 text-md font-bold
                 bg-[rgba(24,24,24,0.71)] border focus:outline-none focus-visible:ring-2 hover:bg-[rgba(24,24,24,0.85)]
                 text-green-500 border-green-500 ring-green-500/30
                 scroll-smooth
