e26fc167dcd15e3ba14ff936ef15153752da682c
Geer68 <germhidalgo@gmail.com>
2025-09-05 14:15:15 -0300

    fixed metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import CountDownSection from "@/components/CountDown";
 import Timeline from "@/components/timeline/Timeline";
 import Hero from "../components/hero/Hero";
